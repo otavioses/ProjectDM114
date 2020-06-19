f3af829fa949189aaceb11ca2f7ba3475a3ad178
Otávio Souza <otavio.souza@watchguard.com>
2020-06-19 16:01:09 -0300

    Implement user authentication

--- app/src/main/java/br/com/osouza/projectdm114/MainActivity.kt
+++ app/src/main/java/br/com/osouza/projectdm114/MainActivity.kt
@@ -1,11 +1,54 @@
 package br.com.osouza.projectdm114
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+
+private const val AUTHENTICATION_REQUEST_CODE = 1
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseApp.initializeApp(this)
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            val name = user.displayName
+            val email = user.email
+            Toast.makeText(this, "Sign in!\nUser: $name \nEmail: $email", Toast.LENGTH_SHORT).show()
+            setContentView(R.layout.activity_main)
+        } else {
+            val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(), AUTHENTICATION_REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == AUTHENTICATION_REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+                if (user != null) {
+                    val name = user.displayName
+                    val email = user.email
+                    Toast.makeText(this, "Sign in!\nUser: $name \nEmail: $email", Toast.LENGTH_SHORT).show()
+                }
+                setContentView(R.layout.activity_main)
+            } else {
+                Toast.makeText(this, "Sign in failed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
